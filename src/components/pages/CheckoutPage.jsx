ec4f383c42cc216abf246f1b4d9af658895a2f3b
gregorytcheng <gregorytcheng@gmail.com>
2020-06-03 15:47:32 -0700

    Added a preliminary layout of checkout page

--- src/components/pages/CheckoutPage.jsx
+++ src/components/pages/CheckoutPage.jsx
@@ -1,8 +1,42 @@
-import React from "react";
-import { Container } from "semantic-ui-react";
+import React, { useContext } from "react";
+import { Container, Grid, List, Header, Image } from "semantic-ui-react";
+import CartContext from "../../contexts/CartContext";
+import Price from "../Price";
 
 const CheckoutPage = () => {
-  return <Container>checkout</Container>;
+  const cart = useContext(CartContext);
+
+  return (
+    <Container>
+      <Grid columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as="h1">Card</Header>
+          </Grid.Column>
+          <Grid.Column>
+            <Header as="h2">Price</Header>
+          </Grid.Column>
+        </Grid.Row>
+        {cart.cartItems.map((cartItem) => (
+          <Grid.Row key={cartItem.id}>
+            <Grid.Column>
+              <List>
+                <List.Item>
+                  <Header>{cartItem.name}</Header>
+                </List.Item>
+                <List.Item>
+                  <Image src={cartItem.avatar} size="tiny" />
+                </List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column>
+              <Price price={cartItem.price * cartItem.quantity} />
+            </Grid.Column>
+          </Grid.Row>
+        ))}
+      </Grid>
+    </Container>
+  );
 };
 
 export default CheckoutPage;
